8a475e1b83e60149ba42f1bc73c8ad91db3d62e0
Megan Cody <Aero@Megans-MacBook-Air.local>
2017-05-30 18:50:38 -0400

    invited and play buttons

--- public/js/main.js
+++ public/js/main.js
@@ -65,7 +65,7 @@ socket.on('join_room_response',function(payload){
 		nodeB.append('<h4>'+payload.username+'</h4>');
 
 		nodeC.addClass('col-3 text-left');
-		var buttonC = makeInviteButton();
+		var buttonC = makeInviteButton(payload.socket_id);
 		nodeC.append(buttonC);
 
 		nodeA.hide();
@@ -77,7 +77,7 @@ socket.on('join_room_response',function(payload){
 		nodeC.slideDown(1000);
 	}
 	else{
-		var buttonC = makeInviteButton();
+		var buttonC = makeInviteButton(payload.socket_id);
 		$('.socket_'+payload.socket_id+' button').replaceWith(buttonC);
 		dom_elements.slideDown(1000);
 	}
@@ -119,6 +119,34 @@ socket.on('player_disconnected',function(payload){
 	newNode.slideDown(1000);
 });
 
+function invite(who){
+	var payload = {};
+	payload.requested_user = who;
+
+	console.log('*** Client Log Message: \'invite\' payload: '+JSON.stringify(payload));
+	socket.emit('invite',payload);
+}
+
+socket.on('invite_response',function(payload){
+	console.log('here');
+	if(payload.result == 'fail'){
+		alert(payload.message);
+		return;
+	}
+	var newNode = makeInvitedButton(payload.socket_id);
+	$('.socket_'+payload.socket_id+' button').replaceWith(newNode);
+});
+
+socket.on('invited',function(payload){
+	console.log('here');
+	if(payload.result == 'fail'){
+		alert(payload.message);
+		return;
+	}
+	var newNode = makePlayButton();
+	$('.socket_'+payload.socket_id+' button').replaceWith(newNode);
+});
+
 socket.on('send_message_response',function(payload){
 	console.log('here');
 	if(payload.result == 'fail'){
@@ -137,13 +165,33 @@ function send_message(){
 	socket.emit('send_message',payload);
 }
 
-function makeInviteButton(){
+function makeInviteButton(socket_id){
 	var newHTML = '<button type=\'button\' class=\'btn btn-outline-primary\'>Invite</button>';
+	var newNode = $(newHTML);
+	newNode.click(function(){
+		invite(socket_id);
+	});
+
+	return(newNode);
+}
+
+function makeInvitedButton(){
+	var newHTML = '<button type=\'button\' class=\'btn btn-primary\'>Invited</button>';
 	var newNode = $(newHTML);
 	return(newNode);
 }
 
+function makePlayButton(){
+	var newHTML = '<button type=\'button\' class=\'btn btn-success\'>Play</button>';
+	var newNode = $(newHTML);
+	return(newNode);
+}
 
+function makeEngageButton(){
+	var newHTML = '<button type=\'button\' class=\'btn btn-danger\'>Engaged</button>';
+	var newNode = $(newHTML);
+	return(newNode);
+}
 
 $(function(){
 	var payload = {};
